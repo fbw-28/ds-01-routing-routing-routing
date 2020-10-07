c393cf59ebfbe54677f824b4685d8ae2d8aee337
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-10-07 14:39:11 +0200

    completed first part of the exercise

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require("express");
 
 const dciModules = require("./data");
 
-const port = 5001;
-
 const app = express();
 
+const port = 5000;
+
 app.listen(port, () => {
-  console.log("server initiated successfully");
+  console.log(`server initiated successfully on port ${port}`);
+});
+
+app.get("/html", (req, res) => {
+  res.send("<h1>Welcome, I am HTML</h1>");
+});
+
+app.get(`/dciModules/`, (req, res) => {
+  console.log("requested whole array");
+  res.json(dciModules);
 });
