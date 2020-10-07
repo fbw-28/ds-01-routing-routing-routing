64d66608efcf75c7d92f8b2614bbae3252c8eae5
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-10-07 15:21:25 +0200

    adding home button to each of the tabs

--- server.js
+++ server.js
@@ -6,15 +6,31 @@ const app = express();
 
 const port = 5000;
 
+app.get("/", (req, res) => {
+  res.send(`<h1>DCI modules as...</h1> 
+      <p><a href="/html">HTML</a></p>
+      <p><a href="/array">Array</a></p>
+      <p><a href="/string">String</a></p>
+  `);
+});
+
 app.listen(port, () => {
-  console.log(`server initiated successfully on port ${port}`);
+  console.log(`server initiated with success on port ${port}`);
 });
 
 app.get("/html", (req, res) => {
-  res.send("<h1>Welcome, I am HTML</h1>");
+  res.send(`<h1>Welcome, I am HTML</h1>
+  <button><a href="/">Home</a></button>`);
 });
 
-app.get(`/dciModules/`, (req, res) => {
-  console.log("requested whole array");
-  res.json(dciModules);
+app.get("/array", (req, res) => {
+  console.log("requested entire array");
+  res.send(dciModules);
+});
+
+const modulesString = dciModules.join(", ");
+
+app.get("/string", (req, res) => {
+  console.log("requested whole array as string");
+  res.send(modulesString);
 });
