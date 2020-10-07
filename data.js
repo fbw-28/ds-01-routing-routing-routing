0ed97950e8b43cfededdf0fbd2c651d8a598fb1a
wuberde <wuberde@gmail.com>
2020-10-08 00:38:57 +0200

    01-done

---
+++ data.js
@@ -0,0 +1,17 @@
+const courses = [
+    'HTML/CSS',
+    'JavaScript',
+    'DOM',
+    'NodeJS',
+    'React',
+    'Data Server',
+    'Fullstack Server',
+    'Final Project',
+  ];
+  
+  module.exports = courses;
+  
+  // or
+  /* module.exports.courses=['HTML/CSS', 'JavaScript', 'DOM', 'NodeJS', 'React', 'Data Server', 'Fullstack Server', 'Final Project'];
+   */
+  
