0ace3f6218c3c9be2ef14d784517ee1516e46cde
LenaIT <olena.denysova@gmail.com>
2020-10-07 14:50:22 +0200

    after presentation

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const courseModules = require('./data')
 const app = express()
 const port = 5000
 
-const text = courseModules.join(", ") 
+/* 
+const list = courseModules.map(item => {
+    if (item === "Data Server") {
+        return `<b><li>${item}</li></b>` 
+    } else {
+        return `<li>${item}</li>`
+}}).join("") */
 
-const list = courseModules.map(item =>
-    item === "Data Server" ? `<b><li>${item}</li></b>` : `<li>${item}</li>`).join("")
-    
         
 //to start server
 //to start browser npm run dev and http://localhost:5000/
@@ -22,12 +25,16 @@ app.get('/', (req, res) => {
 
 //bonus convert to string
 app.get('/text', (req, res) => {
+    const text = courseModules.join(", ") 
     console.log("Route with string")
-    res.json(text)
+    res.send(text)
 })
 
 //bonus html list
 app.get('/html', (req, res) => {
+    const list = courseModules.map(item => {
+        return item === "Data Server" ? `<b><li>${item}</li></b>` : `<li>${item}</li>`
+    }).join("")
     console.log("Route with html list")
     res.send(list)
 })
