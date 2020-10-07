b062b64843da8d21e054827e53047b21965f2a8f
LenaIT <olena.denysova@gmail.com>
2020-10-07 13:45:58 +0200

    with two bonuses

--- server.js
+++ server.js
@@ -3,16 +3,32 @@ const courseModules = require('./data')
 const app = express()
 const port = 5000
 
+const text = courseModules.join(", ") 
 
-
-
+const list = courseModules.map(item =>
+        `<li>${item}</li>`
+).join("")
+    
+        
 //to start server
 //to start browser npm run dev and http://localhost:5000/
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 })
 
-app.get('/json', (req, res) => {
+app.get('/', (req, res) => {
     console.log("Route with all datas call")
     res.json(courseModules)
+})
+
+//bonus convert to string
+app.get('/text', (req, res) => {
+    console.log("Route with string")
+    res.json(text)
+})
+
+//bonus html list
+app.get('/html', (req, res) => {
+    console.log("Route with html list")
+    res.send(list)
 })
